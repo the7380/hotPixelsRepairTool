35800cf3d95ed1a3d0036220adb5f44228de37fc
the7380 <the7110@gmail.com>
2021-02-22 13:12:53 +0300

    Dictionaries are now used to handle CLI parameters

--- __main__.py
+++ __main__.py
@@ -7,15 +7,19 @@ if __name__ == '__main__':
     cli_params = sys.argv[1:]
 
     if "detect" in cli_params:
+        params = dict()
+
         index = cli_params.index("detect")
         if index + 1 < len(cli_params) and cli_params[index + 1] == "--highlight":
-            bpm.start(highlight=True)
-        else:
-            bpm.start()
+            params['highlight'] = True
+
+        bpm.start(**params)
 
     if "fix" in cli_params:
+        params = dict()
+
         index = cli_params.index("fix")
         if index + 1 < len(cli_params) and cli_params[index + 1] == "--replace":
-            bpf.start(replace_mode=True)
-        else:
-            bpf.start()
+            params['replace_mode'] = True
+
+        bpf.start(**params)
